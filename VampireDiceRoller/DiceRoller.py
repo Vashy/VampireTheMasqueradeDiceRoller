0db8342ddc0e7a5d5f2cc68d9b7483bff807e3dc
Timoty Granziero <timoty.granziero@gmail.com>
2021-01-18 22:26:28 +0100

    RollResult

--- VampireDiceRoller/DiceRoller.py
+++ VampireDiceRoller/DiceRoller.py
@@ -5,10 +5,10 @@ from typing import Callable, Final, List
 Randomize: Final = Callable[[int, int], int]
 
 
-class RollResult:
+class RollCount:
     def __init__(self, normal_dices: int, hunger_dices: int, randomize: Randomize):
-        (normal_dices_results, hunger_dices_results) = RollResult.calculate_rolls(normal_dices, hunger_dices,
-                                                                                  randomize)
+        (normal_dices_results, hunger_dices_results) = RollCount.calculate_rolls(normal_dices, hunger_dices,
+                                                                                 randomize)
 
         normal_dices_results = self._map_dices(normal_dices_results)
         hunger_dices_results = self._map_dices(hunger_dices_results)
@@ -19,7 +19,7 @@ class RollResult:
         self.failures = (normal_dices + hunger_dices) - \
                         (self.successes + self.critical_successes + self.messy_criticals + self.bestial_failures)
 
-    class ResultType(Enum):
+    class RollType(Enum):
         SUCCESS = 1
         CRITICAL_SUCCESS = 2
         FAILURE = 3
@@ -36,34 +36,34 @@ class RollResult:
         return normal_dices_results, hunger_dices_results
 
     @staticmethod
-    def _map_dices(numbers: List[int]) -> List[ResultType]:
+    def _map_dices(numbers: List[int]) -> List[RollType]:
         results = []
         for n in numbers:
             if n == 10:
-                results.append(RollResult.ResultType.CRITICAL_SUCCESS)
+                results.append(RollCount.RollType.CRITICAL_SUCCESS)
             elif n >= 6:
-                results.append(RollResult.ResultType.SUCCESS)
+                results.append(RollCount.RollType.SUCCESS)
             elif n == 1:
-                results.append(RollResult.ResultType.CRITICAL_FAILURE)
+                results.append(RollCount.RollType.CRITICAL_FAILURE)
             else:
-                results.append(RollResult.ResultType.FAILURE)
+                results.append(RollCount.RollType.FAILURE)
 
         return results
 
     @staticmethod
-    def _count_successes(roll_results: List[ResultType]) -> int:
-        return RollResult._count(RollResult.ResultType.SUCCESS, roll_results)
+    def _count_successes(roll_results: List[RollType]) -> int:
+        return RollCount._count(RollCount.RollType.SUCCESS, roll_results)
 
     @staticmethod
-    def _count_critical_successes(roll_results: List[ResultType]) -> int:
-        return RollResult._count(RollResult.ResultType.CRITICAL_SUCCESS, roll_results)
+    def _count_critical_successes(roll_results: List[RollType]) -> int:
+        return RollCount._count(RollCount.RollType.CRITICAL_SUCCESS, roll_results)
 
     @staticmethod
     def _count_bestial_failures(hunger_dices_results):
-        return RollResult._count(RollResult.ResultType.CRITICAL_FAILURE, hunger_dices_results)
+        return RollCount._count(RollCount.RollType.CRITICAL_FAILURE, hunger_dices_results)
 
     @staticmethod
-    def _count(result_type: ResultType, roll_results: List[ResultType]) -> int:
+    def _count(result_type: RollType, roll_results: List[RollType]) -> int:
         result = 0
         for roll_result in roll_results:
             if roll_result == result_type:
@@ -71,5 +71,22 @@ class RollResult:
         return result
 
 
-def roll(normal_dices: int, hunger_dices: int = 0, randomize: Randomize = RollResult.calculate_rolls) -> RollResult:
-    return RollResult(normal_dices, hunger_dices, randomize)
+def to_successes(critical_successes: int) -> int:
+    single_successes = critical_successes % 2
+    return (critical_successes - single_successes) * 2 + single_successes
+
+
+class RollResult:
+    def __init__(self, roll_count: RollCount):
+        self.successes = roll_count.successes + \
+                         to_successes(roll_count.critical_successes) + \
+                         to_successes(roll_count.messy_criticals)
+        self.is_messy_critical = roll_count.messy_criticals > 0
+
+
+def roll(normal_dices: int, hunger_dices: int = 0, randomize: Randomize = RollCount.calculate_rolls) -> RollCount:
+    return RollCount(normal_dices, hunger_dices, randomize)
+
+
+def stringify(roll_count: RollCount) -> str:
+    pass
--- VampireDiceRoller/test/DiceRollerTest.py
+++ VampireDiceRoller/test/DiceRollerTest.py
@@ -1,6 +1,6 @@
 import unittest
 
-from VampireDiceRoller.DiceRoller import roll
+from VampireDiceRoller.DiceRoller import roll, RollResult
 
 
 class MyTestCase(unittest.TestCase):
@@ -14,25 +14,42 @@ class MyTestCase(unittest.TestCase):
 
     def test_no_hunger_dices(self):
         result = roll(12, randomize=always_success_fake_randomize)
-        self.assertEqual(result.successes, 12)
-        self.assertEqual(result.failures, 0)
+        self.assertEqual(12, result.successes, 12)
+        self.assertEqual(0, result.failures)
 
     def test_no_successes_randomizer(self):
         result = roll(13, 15, no_successes_fake_randomize)
-        self.assertEqual(result.successes, 0)
-        self.assertEqual(result.failures, 28)
+        self.assertEqual(0, result.successes)
+        self.assertEqual(28, result.failures)
 
     def test_ascending_randomize(self):
         result = roll(10, 10, AscendingFakeRandomize().randomize)
-        self.assertEqual(result.successes, 4 + 4)
-        self.assertEqual(result.failures, 5 + 4)
-        self.assertEqual(result.critical_successes, 1)
-        self.assertEqual(result.messy_criticals, 1)
-        self.assertEqual(result.bestial_failures, 1)
+        self.assertEqual(4 + 4, result.successes)
+        self.assertEqual(5 + 4, result.failures)
+        self.assertEqual(1, result.critical_successes)
+        self.assertEqual(1, result.messy_criticals)
+        self.assertEqual(1, result.bestial_failures)
+
+    def test_roll_result_successes_and_criticals(self):
+        result = RollResult(roll(4, 1, always_critical_fake_randomize))
+        self.assertEqual(9, result.successes)
+        self.assertEqual(True, result.is_messy_critical)
+        result = RollResult(roll(8, 2, always_critical_fake_randomize))
+        self.assertEqual(20, result.successes)
+        self.assertEqual(True, result.is_messy_critical)
+
+    def test_roll_result_failures_and_successes(self):
+        result = RollResult(roll(5, 4, AscendingFakeRandomize().randomize))
+        self.assertEqual(4, result.successes)
+        self.assertEqual(False, result.is_messy_critical)
 
 
 def always_success_fake_randomize(ignored1, ignored2) -> int:
     return 9
+
+
+def always_critical_fake_randomize(ignored1, ignored2) -> int:
+    return 10
 
 
 def no_successes_fake_randomize(ignored1, ignored2) -> int:
