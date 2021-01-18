645cb1bc52806903d842d59eaf51af0c31fe96f0
Timoty Granziero <timoty.granziero@gmail.com>
2021-01-18 21:39:58 +0100

    Renames

--- VampireDiceRoller/test/DiceRollerTest.py
+++ VampireDiceRoller/test/DiceRollerTest.py
@@ -6,25 +6,24 @@ from VampireDiceRoller.DiceRoller import roll
 class MyTestCase(unittest.TestCase):
 
     def test_always_success_randomizer(self):
-        result = roll(3, 5, always_success)
+        result = roll(3, 5, always_success_fake_randomize)
         self.assertEqual(result.successes, 8)
         self.assertEqual(result.failures, 0)
         self.assertEqual(result.critical_successes, 0)
         self.assertEqual(result.bestial_failures, 0)
 
     def test_no_hunger_dices(self):
-        result = roll(12, randomize=always_success)
+        result = roll(12, randomize=always_success_fake_randomize)
         self.assertEqual(result.successes, 12)
         self.assertEqual(result.failures, 0)
 
     def test_no_successes_randomizer(self):
-        result = roll(13, 15, no_successes)
+        result = roll(13, 15, no_successes_fake_randomize)
         self.assertEqual(result.successes, 0)
         self.assertEqual(result.failures, 28)
 
     def test_ascending_randomize(self):
-        randomize = AscendingRandomize()
-        result = roll(10, 10, randomize.ascending_randomize)
+        result = roll(10, 10, AscendingFakeRandomize().randomize)
         self.assertEqual(result.successes, 4 + 4)
         self.assertEqual(result.failures, 5 + 4)
         self.assertEqual(result.critical_successes, 1)
@@ -32,19 +31,19 @@ class MyTestCase(unittest.TestCase):
         self.assertEqual(result.bestial_failures, 1)
 
 
-def always_success(ignored1, ignored2) -> int:
+def always_success_fake_randomize(ignored1, ignored2) -> int:
     return 9
 
 
-def no_successes(ignored1, ignored2) -> int:
+def no_successes_fake_randomize(ignored1, ignored2) -> int:
     return 2
 
 
-class AscendingRandomize:
+class AscendingFakeRandomize:
     def __init__(self):
         self.value = 0
 
-    def ascending_randomize(self, ignored1, ignored2) -> int:
+    def randomize(self, ignored1, ignored2) -> int:
         if self.value == 10:
             self.value = 0
         self.value += 1
