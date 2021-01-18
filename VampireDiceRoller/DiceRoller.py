3fcf14205c2948f83476ad2af1b4e6da8cba6a77
Timoty Granziero <timoty.granziero@gmail.com>
2021-01-18 15:19:12 +0100

    Implement successes and failures

--- VampireDiceRoller/DiceRoller.py
+++ VampireDiceRoller/DiceRoller.py
@@ -1,16 +1,77 @@
+from enum import Enum
 from random import randint
-from typing import Callable, Final
+from typing import Callable, Final, List
 
 Randomize: Final = Callable[[int, int], int]
 
 
-def calculate_successes(n: int, randomize: Randomize = randint) -> int:
-    successes = 0
-    for _ in range(n):
-        if randomize(1, 10) >= 6:
-            successes += 1
-    return successes
+def calculate_rolls(normal_dices: int, hunger_dices: int, randomize: Randomize = randint) -> (int, int):
+    normal_dices_results = []
+    for _ in range(normal_dices):
+        normal_dices_results.append(randomize(1, 10))
+    hunger_dices_results = []
+    for _ in range(hunger_dices):
+        hunger_dices_results.append(randomize(1, 10))
+    return normal_dices_results, hunger_dices_results
 
 
-def roll(normal_dices: int, hunger_dices: int = 0, randomize=calculate_successes) -> (int, int):
-    return [randomize(normal_dices), randomize(hunger_dices)]
+class RollResult:
+    def __init__(self, successes, failures, critical_successes, bestial_criticals):
+        self._successes = successes
+        self._failures = failures
+        self._critical_successes = critical_successes
+        self._bestial_criticals = bestial_criticals
+
+    @property
+    def successes(self):
+        return self._successes
+
+    @property
+    def failures(self):
+        return self._failures
+
+    @property
+    def critical_successes(self):
+        return self._critical_successes
+
+    @property
+    def bestial_criticals(self):
+        return self._bestial_criticals
+
+    class ResultType(Enum):
+        SUCCESS = 1
+        CRITICAL_SUCCESS = 2
+        FAILURE = 3
+        BESTIAL_FAILURE = 4
+        BESTIAL_CRITICAL = 5
+
+
+def map_dices(numbers: List[int]) -> List[RollResult.ResultType]:
+    results = []
+    for n in numbers:
+        if n == 10:
+            results.append(RollResult.ResultType.CRITICAL_SUCCESS)
+        elif n > 6:
+            results.append(RollResult.ResultType.SUCCESS)
+        else:
+            results.append(RollResult.ResultType.FAILURE)
+
+    return results
+
+
+def count_successes(roll_results: List[RollResult.ResultType]) -> int:
+    result = 0
+    for roll_result in roll_results:
+        if roll_result == RollResult.ResultType.SUCCESS:
+            result += 1
+    return result
+
+
+def roll(normal_dices: int, hunger_dices: int = 0, randomize: Randomize = calculate_rolls) -> RollResult:
+    (normal_dices_results, hunger_dices_results) = calculate_rolls(normal_dices, hunger_dices, randomize)
+
+    normal_dices_results = map_dices(normal_dices_results)
+    successes = count_successes(normal_dices_results)
+    # bestial_successes = count_successes(hunger_dices_results)
+
+    return RollResult(successes, normal_dices - successes, 0, 0)
--- VampireDiceRoller/test/DiceRollerTest.py
+++ VampireDiceRoller/test/DiceRollerTest.py
@@ -6,27 +6,27 @@ from VampireDiceRoller.DiceRoller import roll
 class MyTestCase(unittest.TestCase):
 
     def test_always_success_randomizer(self):
-        result = roll(1, 5, always_success)
-        self.assertEqual(result[0], 1)
-        self.assertEqual(result[1], 5)
+        result = roll(3, 5, always_success)
+        self.assertEqual(result.successes, 3)
+        self.assertEqual(result.failures, 0)
 
     def test_no_hunger_dices(self):
         result = roll(12, randomize=always_success)
-        self.assertEqual(result[0], 12)
-        self.assertEqual(result[1], 0)
+        self.assertEqual(result.successes, 12)
+        self.assertEqual(result.failures, 0)
 
     def test_no_successes_randomizer(self):
         result = roll(13, 15, no_successes)
-        self.assertEqual(result[0], 0)
-        self.assertEqual(result[1], 0)
+        self.assertEqual(result.successes, 0)
+        self.assertEqual(result.failures, 13)
 
 
-def always_success(n: int) -> int:
-    return n
+def always_success(ignored1, ignored2) -> int:
+    return 9
 
 
-def no_successes(n) -> int:
-    return 0
+def no_successes(ignored1, ignored2) -> int:
+    return 1
 
 
 if __name__ == '__main__':
